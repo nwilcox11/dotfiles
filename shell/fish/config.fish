f97d37b5b5a490a193343e3d757b2070ef4dbf3d
nwilcox11 <wilcox.nicholas.m@gmail.com>
2019-12-20 00:31:07 -0500

    adds base16 script to fish.

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -3,7 +3,7 @@ set __fish_git_prompt_showuntrackedfiles 'yes'
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate ''
 set __fish_git_prompt_showupstream 'none'
-set -g fish_prompt_pwd_dir_length 3
+set -g fish_prompt_pwd_dir_length 5
 
 # Fish should not add things to clipboard when killing
 # See https://github.com/fish-shell/fish-shell/issues/772
@@ -11,9 +11,14 @@ set FISH_CLIPBOARD_CMD "cat"
 
 set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
 
+# Base16 Shell
+if status --is-interactive
+    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
+end
+
 function fish_prompt
 	set_color brblack
-	#echo -n "["(date "+%H:%M")"] "
+	# echo -n "["(date "+%H:%M")"] "
 	set_color blue
 	echo -n (hostname)
 	if [ $PWD != $HOME ]
