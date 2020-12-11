955a04af82390906e895c9a38b6672750fbaa647
nwilcox11 <wilcox.nicholas.m@gmail.com>
2020-12-11 14:12:59 -0500

    adds llvm (clangd) to path.

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -9,12 +9,6 @@ set -g fish_prompt_pwd_dir_length 3
 # See https://github.com/fish-shell/fish-shell/issues/772
 set FISH_CLIPBOARD_CMD "cat"
 
-
-# Base16 Shell
-if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
-end
-
 # Fish paths
 set -U fish_user_paths /usr/local/sbin /usr/local/bin /usr/bin /bin
 # rust
@@ -23,6 +17,8 @@ set PATH $HOME/.cargo/bin $PATH
 set PATH /usr/local/psql/bin $PATH
 # postgres data
 set PATH /usr/local/var/postgres/data $PATH
+# clangd lsp
+set PATH /usr/local/opt/llvm/bin $PATH
 
 function fish_prompt
 	set_color brblack
