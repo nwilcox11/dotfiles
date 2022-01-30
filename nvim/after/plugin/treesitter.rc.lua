f52948e75e022e2a2e70b5380a1c9a24697adaee
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-01-30 14:46:56 -0500

    Simplify treesitter config.

--- nvim/after/plugin/treesitter.rc.lua
+++ nvim/after/plugin/treesitter.rc.lua
@@ -1,30 +1,28 @@
 local has_ts, ts = pcall(require, 'nvim-treesitter.configs')
+-- https://github.com/nvim-treesitter/nvim-treesitter
 
 if has_ts then
-    ts.setup {
-        ensure_installed = {
-            "typescript",
-            "tsx",
-            "go",
+  ts.setup {
+    ensure_installed = {
+      "typescript",
+      "tsx",
+      "go",
 			"lua",
-            "rust",
-            "python",
-            "c",
-            "toml",
-            "fish",
-            "json",
-            "html",
-            "css"
-        },
-	additional_vim_regex_highlighting = false,
-        highlight = {
-            enable = true,
-            disable = {},
-        },
-
-        indent = {
-            enable = true,
-            disable = { "typescript", "python" }
-        },
-    }
+      "rust",
+      "python",
+      "c",
+      "toml",
+      "fish",
+      "json",
+      "html",
+      "css"
+    },
+    highlight = {
+      enable = false
+    },
+    indent = {
+      enable = true,
+      disable = { "typescript", "python" }
+    },
+   }
 end
