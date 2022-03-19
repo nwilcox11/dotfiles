36929447550cf5250d2c0d3511edb87a758b00e0
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-03-19 16:57:15 -0400

    Updates auto complete setup.

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -1,28 +1,40 @@
 local has_cmp, cmp = pcall(require, "cmp")
 
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
 if has_cmp then
+  local lspkind = require"lspkind"
+  lspkind.init()
+
   cmp.setup {
     mapping = {
-      ['<C-p>'] = cmp.mapping.select_prev_item(),
-      ['<C-n>'] = cmp.mapping.select_next_item(),
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      },
-       -- ['<Tab>'] = function(fallback)
-       --   if cmp.visible() then
-       --     cmp.select_next_item()
-       --   else
-       --     fallback()
-       --   end
-       -- end,
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<c-y>"] = cmp.mapping(
+          cmp.mapping.confirm {
+              behavior = cmp.ConfirmBehavior.Insert,
+              select = true,
+          },
+          { "i", "c" }
+      ),
     },
+
     sources = {
-      { name = 'nvim_lsp' },
+      { name = "nvim_lsp" },
+      { name = "path" },
+      { name = "buffer" },
     },
-  }
+
+    formatting = {
+      format = lspkind.cmp_format({
+        mode = "symbol_text",
+        menu = ({
+          buffer = "[buf]",
+          nvim_lsp = "[LSP]",
+          path = "[path]"
+        })
+      })
+    },
+}
 end
