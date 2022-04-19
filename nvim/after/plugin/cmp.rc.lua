a079763914d5a72206f67f63d4f5bf2611bb660b
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-04-18 22:03:30 -0400

    Adds luasnip, fixes errors.

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -3,38 +3,44 @@ local has_cmp, cmp = pcall(require, "cmp")
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 if has_cmp then
-  local lspkind = require"lspkind"
-  lspkind.init()
+    local lspkind = require"lspkind"
+    lspkind.init()
 
-  cmp.setup {
-    mapping = {
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<c-y>"] = cmp.mapping(
-          cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Insert,
-              select = true,
-          },
-          { "i", "c" }
-      ),
-    },
+    cmp.setup {
+         mapping = {
+           ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+           ["<C-e>"] = cmp.mapping.close(),
+           ["C-space"] = cmp.mapping.complete(),
+           ["<CR>"] = cmp.mapping.confirm(),
+         },
 
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "path" },
-      { name = "buffer" },
-    },
+        snippet = {
+          expand = function(args)
+            require"luasnip".lsp_expand(args.body)
+          end,
+        },
 
-    formatting = {
-      format = lspkind.cmp_format({
-        mode = "symbol_text",
-        menu = ({
-          buffer = "[buf]",
-          nvim_lsp = "[LSP]",
-          path = "[path]"
-        })
-      })
-    },
-}
+        sources = {
+            { name = "nvim_lsp" },
+            { name = "path" },
+            { name = "buffer" },
+        },
+
+        formatting = {
+            format = lspkind.cmp_format({
+                mode = "symbol_text",
+                menu = ({
+                    buffer = "[buf]",
+                    nvim_lsp = "[LSP]",
+                    path = "[path]"
+                })
+            })
+        },
+
+        experimental = {
+            native_menu = false,
+            ghost_text = false
+        }
+    }
 end
