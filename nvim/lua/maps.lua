37e5c3cda3bb34325b7b765feec08e248b9c6016
Nick Wilcox <nicholas.wilcox@ge.com>
2022-07-05 12:10:31 -0400

    Tabs

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -5,8 +5,8 @@ vim.api.nvim_set_keymap("n", "<Leader>j", ":wincmd j<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>k", ":wincmd k<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Leader>l", ":wincmd l<CR>", opts)
 -- Tab management
-vim.api.nvim_set_keymap("n", "th", ":tabprevious<CR>", opts)
-vim.api.nvim_set_keymap("n", "tl", ":tabnext<CR>", opts)
+vim.api.nvim_set_keymap("n", "H", ":tabprevious<CR>", opts)
+vim.api.nvim_set_keymap("n", "L", ":tabnext<CR>", opts)
 vim.api.nvim_set_keymap("n", "tn", ":tabnew<CR>", opts)
 vim.api.nvim_set_keymap("n", "ts", ":tab split<CR>", opts)
 
@@ -17,3 +17,6 @@ vim.api.nvim_set_keymap("n", "<C-c>", "<esc>", opts)
 vim.api.nvim_set_keymap("n", "<C-q>", "<Cmd> :copen<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-k>", "<Cmd> :cnext<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-j>", "<Cmd> :cprev<CR>", opts)
+
+-- Hold on to what I have yanked after paste.
+vim.api.nvim_set_keymap("v", "p", '"_dP"', opts)
