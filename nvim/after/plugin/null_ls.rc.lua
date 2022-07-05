8495f0e390d506d4b8a6830fab71037412549b66
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-07-05 10:37:52 -0400

    Simplify

---
+++ nvim/after/plugin/null_ls.rc.lua
@@ -0,0 +1,19 @@
+local null_ok, null = pcall(require, "null-ls")
+if not null_ok then
+  return
+end
+
+local formatting_file_types = { "json", "javascript", "javascriptreact", "typescript", "typescriptreact" }
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null.builtins.diagnostics
+
+null.setup({
+  sources = {
+    formatting.prettier.with({
+     filetypes = formatting_file_types
+    }),
+    diagnostics.eslint_d,
+  },
+})
