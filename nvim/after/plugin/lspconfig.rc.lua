5b58e58782992f32abc2a4784c769b2aff70d982
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-11-03 12:09:39 -0400

    Auto format for supported LSP's

--- nvim/after/plugin/lspconfig.rc.lua
+++ nvim/after/plugin/lspconfig.rc.lua
@@ -28,6 +28,20 @@ if has_lsp then
     end
 
     lsp_keymaps(bufnr)
+
+    -- Auto format on save for supported LSP's
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              vim.lsp.buf.formatting_sync()
+          end,
+      })
+    end
   end
 
   local diagnostic_signs = {
