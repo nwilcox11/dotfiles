42599af2c0b563493ec1a4e8bf885148f50fdc04
Nick Wilcox <nicholas.wilcox@ge.com>
2022-12-19 12:53:02 -0500

    chore: adds git log keymap.

--- nvim/after/plugin/fugitive.rc.lua
+++ nvim/after/plugin/fugitive.rc.lua
@@ -1,3 +1,3 @@
--- vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {});
 local opts = { silent = true }
 vim.api.nvim_set_keymap("n", "<Leader>gs", ":Git <CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>gl", ":Gclog <CR>", opts)
