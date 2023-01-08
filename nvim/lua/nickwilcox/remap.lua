e7282908661ac3995feed2c75f99bf8137791794
nwilcox11 <wilcox.nicholas.m@gmail.com>
2023-01-08 10:59:55 -0500

    fix: <esc> should be silent

--- nvim/lua/nickwilcox/remap.lua
+++ nvim/lua/nickwilcox/remap.lua
@@ -1,7 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
-vim.keymap.set("i", "<C-c>", "<Esc>");
-vim.keymap.set("n", "Q", "<nop>");
-
 -- Move highlighted text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -41,3 +37,7 @@ vim.keymap.set("n", "<Leader>k", ":wincmd k<CR>")
 vim.keymap.set("n", "<Leader>l", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
+vim.keymap.set({ "i", "n" }, "<C-c>", "<Esc>", { silent = true });
+vim.keymap.set("n", "Q", "<nop>");
