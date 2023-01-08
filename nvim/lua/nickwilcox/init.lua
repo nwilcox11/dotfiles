021476d0e187b83d34fc3476aeedd59f1f82cbe2
nwilcox11 <wilcox.nicholas.m@gmail.com>
2023-01-08 14:06:38 -0500

    fix: cleanup

--- nvim/lua/nickwilcox/init.lua
+++ nvim/lua/nickwilcox/init.lua
@@ -1,6 +1,6 @@
-require("nickwilcox.packer")
 require("nickwilcox.set")
 require("nickwilcox.remap")
+require("nickwilcox.packer")
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
