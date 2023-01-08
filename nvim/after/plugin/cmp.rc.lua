053c77e746a81275421f1ab6e2a62021b0961ec9
nwilcox11 <wilcox.nicholas.m@gmail.com>
2023-01-08 00:33:16 -0500

    feat: Removes lsp-zero, adds custom configs.

---
+++ nvim/after/plugin/cmp.rc.lua
@@ -0,0 +1,41 @@
+local cmp = require("cmp")
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered()
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["C-space"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm(),
+    ["<c-n>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ["<c-p>"] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "buffer" },
+    { name = "luasnip" },
+  }),
+})
--- nvim/lua/nickwilcox/remap.lua
+++ nvim/lua/nickwilcox/remap.lua
@@ -1,4 +1,43 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
 vim.keymap.set("i", "<C-c>", "<Esc>");
 vim.keymap.set("n", "Q", "<nop>");
+
+-- Move highlighted text
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+-- Keeps cursor in the middle of screen while half page jumping.
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keeps search terms in the middle of screen.
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- Quick fix navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Tab Managment
+vim.keymap.set("n", "H", ":tabprevious<CR>")
+vim.keymap.set("n", "L", ":tabnext<CR>")
+vim.keymap.set("n", "tn", ":tabnew<CR>")
+vim.keymap.set("n", "ts", ":tab split<CR>")
+
+-- Split managment
+vim.keymap.set("n", "<Leader>h", ":wincmd h<CR>")
+vim.keymap.set("n", "<Leader>j", ":wincmd j<CR>")
+vim.keymap.set("n", "<Leader>k", ":wincmd k<CR>")
+vim.keymap.set("n", "<Leader>l", ":wincmd l<CR>")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
