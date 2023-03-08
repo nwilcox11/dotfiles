4899790ea44e166f12c0a8ffef3a5ac3f4e3dc6c
nwilcox11 <wilcox.nicholas.m@gmail.com>
2023-03-07 22:46:28 -0500

    feat: adds global lua table inspect

--- nvim/after/plugin/lsp.rc.lua
+++ nvim/after/plugin/lsp.rc.lua
@@ -38,7 +38,7 @@ lsp.lua_ls.setup {
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { 'vim', 'P' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
--- nvim/lua/nickwilcox/init.lua
+++ nvim/lua/nickwilcox/init.lua
@@ -1,6 +1,7 @@
 require("nickwilcox.set")
 require("nickwilcox.remap")
 require("nickwilcox.packer")
+require("nickwilcox.globals")
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
