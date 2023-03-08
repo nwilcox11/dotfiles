b0449d95b3dc6712a4043b446c991548bb6cbcca
nwilcox11 <wilcox.nicholas.m@gmail.com>
2023-03-07 22:47:13 -0500

    feat: adds vim help tag search

--- nvim/after/plugin/telescope.rc.lua
+++ nvim/after/plugin/telescope.rc.lua
@@ -6,9 +6,14 @@ tscope.setup {
     live_grep = {
       theme = "dropdown"
     },
+    help_tags = {
+      theme = "dropdown",
+      previewer = false,
+    },
   },
 }
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
