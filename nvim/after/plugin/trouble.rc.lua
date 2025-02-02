584f395b544459af0b9b00e0bb3743dc0ed34397
nwilcox11 <wilcox.nicholas.m@gmail.com>
2025-02-01 22:17:54 -0500

    fix(trouble): fixes breaking changes.

--- nvim/after/plugin/trouble.rc.lua
+++ nvim/after/plugin/trouble.rc.lua
@@ -1,10 +1,6 @@
 local tr = require("trouble")
-tr.setup {
-  icons = false,
-  use_diagnostic_signs = true,
-}
 
-vim.keymap.set("n", "<leader>xx", tr.toggle)
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>")
 vim.keymap.set("n", "<leader>xq", function() tr.toggle("quickfix") end)
 vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
 vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
