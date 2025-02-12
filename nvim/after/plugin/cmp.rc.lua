5d8ab7647a90732c8f5abc7acd1b57006f1b3a55
nwilcox11 <wilcox.nicholas.m@gmail.com>
2025-02-11 22:12:37 -0500

    chore(nvim): Small updates

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -1,5 +1,7 @@
 local cmp = require("cmp")
 
+vim.opt.shortmess:append "c"
+
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 cmp.setup({
   snippet = {
--- nvim/lua/nickwilcox/set.lua
+++ nvim/lua/nickwilcox/set.lua
@@ -3,6 +3,9 @@ vim.opt.relativenumber = true
 vim.opt.background = "dark"
 vim.opt.cursorline = true
 
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -35,6 +38,8 @@ vim.opt.belloff = "all"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+vim.opt.formatoptions:remove "o"
+
 vim.g.mapleader = " "
 vim.g.markdown_fenced_languages = {
   "ts=typescript"
@@ -43,5 +48,3 @@ vim.g.markdown_fenced_languages = {
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
-
--- vim.cmd('colorscheme gruvbox')
